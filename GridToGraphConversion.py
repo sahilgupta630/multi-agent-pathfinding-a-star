a6cfd4488c8af1bd66b82ddbd9ba5c40d08c71f2
Sahil <guptasahilkumar783@gmail.com>
2026-03-11 02:09:33 +0530

    Refactor: Wrap GridToGraphConversion script execution in a main method

--- GridToGraphConversion.py
+++ GridToGraphConversion.py
@@ -15,38 +15,42 @@ def visualize_graph(graph):
     plot.show()
 
 
-# Define the graph
-graph = {}
+def main():
+    # Define the graph
+    graph = {}
 
-# Define the grid
-grid = [
-    [1, 0, 1, 1, 1, 1, 0, 1, 1, 1],
-    [1, 1, 1, 0, 1, 1, 1, 0, 1, 1],
-    [1, 1, 1, 0, 1, 1, 0, 1, 0, 1],
-    [0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
-    [1, 1, 1, 0, 1, 1, 1, 0, 1, 0],
-    [1, 0, 1, 1, 1, 1, 0, 1, 0, 0],
-    [1, 0, 0, 0, 0, 1, 0, 0, 0, 1],
-    [1, 0, 1, 1, 1, 1, 0, 1, 1, 1],
-    [1, 1, 1, 0, 0, 0, 1, 0, 0, 1]
-]
+    # Define the grid
+    grid = [
+        [1, 0, 1, 1, 1, 1, 0, 1, 1, 1],
+        [1, 1, 1, 0, 1, 1, 1, 0, 1, 1],
+        [1, 1, 1, 0, 1, 1, 0, 1, 0, 1],
+        [0, 0, 1, 0, 1, 0, 0, 0, 0, 1],
+        [1, 1, 1, 0, 1, 1, 1, 0, 1, 0],
+        [1, 0, 1, 1, 1, 1, 0, 1, 0, 0],
+        [1, 0, 0, 0, 0, 1, 0, 0, 0, 1],
+        [1, 0, 1, 1, 1, 1, 0, 1, 1, 1],
+        [1, 1, 1, 0, 0, 0, 1, 0, 0, 1]
+    ]
 
-# Convert grid to graph
-for i in range(len(grid)):
-    for j in range(len(grid[0])):
-        if grid[i][j] == 1:
-            neighbors = []
-            if i > 0 and grid[i - 1][j] == 1:
-                neighbors.append((i - 1, j))
-            if i < len(grid) - 1 and grid[i + 1][j] == 1:
-                neighbors.append((i + 1, j))
-            if j > 0 and grid[i][j - 1] == 1:
-                neighbors.append((i, j - 1))
-            if j < len(grid[0]) - 1 and grid[i][j + 1] == 1:
-                neighbors.append((i, j + 1))
-            graph[(i, j)] = neighbors
+    # Convert grid to graph
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                neighbors = []
+                if i > 0 and grid[i - 1][j] == 1:
+                    neighbors.append((i - 1, j))
+                if i < len(grid) - 1 and grid[i + 1][j] == 1:
+                    neighbors.append((i + 1, j))
+                if j > 0 and grid[i][j - 1] == 1:
+                    neighbors.append((i, j - 1))
+                if j < len(grid[0]) - 1 and grid[i][j + 1] == 1:
+                    neighbors.append((i, j + 1))
+                graph[(i, j)] = neighbors
 
-print(graph)
+    print(graph)
 
-# Visualize the graph
-visualize_graph(graph)
+    # Visualize the graph
+    visualize_graph(graph)
+
+if __name__ == "__main__":
+    main()
